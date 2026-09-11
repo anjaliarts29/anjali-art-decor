cc60871aec38fba47140d951e34944e39bf0bdbb
anjaliarts29 <anjaliarts29@gmail.com>
2026-09-11 10:02:08 -0500

    Update ChatBot.jsx

--- src/ChatBot.jsx
+++ src/ChatBot.jsx
@@ -7,7 +7,7 @@ export default function ChatBot() {
   useEffect(() => {
   const timer = setTimeout(() => {
     setIsOpen(true);
-  }, 3000);
+  }, 5000);
 
   return () => clearTimeout(timer);
 }, []);
